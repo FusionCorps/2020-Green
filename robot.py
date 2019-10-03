f3f280ec439598fddf6568f22eda4b0b801f1052
Winston Hartnett <winstonhartnett@gmail.com>
2019-10-02 19:11:10 -0500

    Base robot.py structure

--- robot.py
+++ robot.py
@@ -1,1 +1,11 @@
+from commandbased import CommandBasedRobot
+from wpilib import run
 
+
+class Green(CommandBasedRobot):
+    def robotInit(self):
+        pass
+
+
+if __name__ == "__main__":
+    run(Green)
