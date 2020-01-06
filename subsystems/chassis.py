fb893cff60e2a8c79c56ed1841fcd418e3feb2f2
Winston Hartnett <winstonhartnett@gmail.com>
2020-01-06 15:36:04 -0600

    Added formatter and linting

--- subsystems/chassis.py
+++ subsystems/chassis.py
@@ -1,10 +1,12 @@
+from rev import CANSparkMax, MotorType
 from wpilib.command import Subsystem
 from wpilib.drive import DifferentialDrive
-from rev import CANSparkMax, MotorType
+
 
 class Chassis(Subsystem):
     def __init__(self):
         super().__init__("Chassis")
+
         self.l_controller = CANSparkMax(0, MotorType.kBrushless)
         self.r_controller = CANSparkMax(1, MotorType.kBrushless)
         self.drive = DifferentialDrive(self.l_controller, self.r_controller)
