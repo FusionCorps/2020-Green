59d9721e396dd3b335dee1ea779c713a132e0154
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-01-15 18:51:16 -0600

    intake subsystem

---
+++ subsystems/intake.py
@@ -0,0 +1,22 @@
+
+import math
+from datetime import datetime, timedelta
+from threading import RLock, Thread
+from time import sleep
+from typing import List
+
+from ctre import WPI_TalonSRX
+
+
+from wpilib import SPI
+from wpilib.command import Subsystem
+from wpilib.drive import DifferentialDrive
+
+
+
+class Intake(Subsystem):
+    def __init__(self):
+        super.__init__("Intake")
+
+    
+
