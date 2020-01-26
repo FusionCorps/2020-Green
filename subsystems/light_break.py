e44336c9b62525d4e9d1a8b75a1585a34a1506c1
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-01-26 11:30:44 -0600

    Started the light breakage logic loop infrastructure.

---
+++ subsystems/light_break.py
@@ -0,0 +1,56 @@
+import board
+import digitalio
+from wpilib.command import Subsystem
+from ctre import WPI_TalonFX
+ 
+# Create digital input with pull-up resistor on pin D5
+# for break beam sensor.
+break_beam = digitalio.DigitalInOut(board.D5)
+break_beam.direction = digitalio.Direction.INPUT
+break_beam.pull = digitalio.Pull.UP
+
+
+
+class Intake(Subsystem):
+    def __init(self):
+        super.__init__('Intake')
+
+        BREAK_BEAM_TOP_ID = 0
+        BREAK_BEAM_VERT_CORNER_ID = 1
+        BREAK_BEAM_HORIZ_CORNER_ID = 2
+        BREAK_BEAM_ENTRY_ID = 3
+
+        VERT_BELT_MOTOR_ID = 13
+        HORIZ_BELT_MOTOR_ID = 14
+
+        self.is_loading = False
+
+        self.ball_count_horiz = 0
+        self.ball_count_vert = 0
+
+        self.break_beam_entry = digitalio.DigitalInOut(board.BREAK_BEAM_ENTRY_ID)
+        self.break_beam_entry.direction = digitalio.Direction.INPUT
+        self.break_beam_entry.pull = digitalio.Pull.UP
+        
+        self.break_beam_horiz_corner = digitalio.DigitalInOut(board.BREAK_BEAM_HORIZ_CORNER_ID)
+        self.break_beam_horiz_corner.direction = digitalio.Direction.INPUT
+        self.break_beam_horiz_corner.pull = digitalio.Pull.UP
+        
+        self.break_beam_vert_corner = digitalio.DigitalInOut(board.BREAK_BEAM_VERT_CORNER_ID)
+        self.break_beam_vert_corner.direction = digitalio.Direction.INPUT
+        self.break_beam_vert_corner.pull = digitalio.Pull.UP
+        
+        self.break_beam_top = digitalio.DigitalInOut(board.BREAK_BEAM_TOP_ID)
+        self.break_beam_top.direction = digitalio.Direction.INPUT
+        self.break_beam_top.pull = digitalio.Pull.UP
+
+        
+    
+    def load_to_top(self):
+        while self.break_beam_top.value:
+
+        
+
+        
+
+
