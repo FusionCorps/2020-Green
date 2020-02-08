910e5d3c6fc3a7bf20e5401c6bc4af89b2400355
Winston Hartnett <winstonhartnett@gmail.com>
2020-02-08 11:18:32 -0600

    Preliminary indexer and shooter stuff

--- commands/__init__.py
+++ commands/__init__.py
@@ -1,6 +1,10 @@
 from inputs.xbox360 import XBoxController
 
 from . import shooter
+from . import indexer
 
 XBoxController().a.whenPressed(shooter.SetPercentage(1.0))
 XBoxController().a.whenReleased(shooter.SetPercentage(0.0))
+
+XBoxController().b.whenPressed(indexer.SetPercentage(1.0))
+XBoxController().b.whenReleased(indexer.SetPercentage(0.0))
