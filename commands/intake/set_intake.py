08d0c0d7c20813e51086ac6897d01c4320822983
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-02-08 10:35:34 -0600

    command fixes

--- commands/intake/set_intake.py
+++ commands/intake/set_intake.py
@@ -4,9 +4,11 @@ from subsystems.intake import Intake
 class SetIntake(InstantCommand):
     def __init__(self, velocity):
         super().__init__("SetIntake")
+        self.requires(Intake())\
+        self.velocity = velocity
 
     def initiate(self):
-        Intake().set_intake(velocity)
+        Intake().set_intake(self.velocity)
     
 
 
--- commands/intake/turn_off.py
+++ commands/intake/turn_off.py
@@ -3,5 +3,9 @@ from subsystems.intake import Intake
 
 class TurnOff(InstantCommand):
 
+    def __init__(self):
+        super().__init__('TurnOff')
+        self.requires(Intake())
+
     def initiate(self):
         Intake().turn_off()
