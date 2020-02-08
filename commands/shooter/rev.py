a4e0360bc0128366b4ad061666157cf8fee1f38e
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-02-08 10:04:05 -0600

    change to percentage

--- commands/shooter/rev.py
+++ commands/shooter/rev.py
@@ -2,10 +2,9 @@ from wpilib.command import InstantCommand
 from subsystems.shooter import Shooter
 
 class Rev(self, velocity):
-    def __init__(self, velocity):
+    def __init__(self, per):
         super().__init__(__name__)
-        self.velocity = velocity
+        self.velocity = per
 
     def initalize(self):
-        Shooter.set_motors_velocity(velocity)
-        
+        Shooter.set_motors_percentage(per)
