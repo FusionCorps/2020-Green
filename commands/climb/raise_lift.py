27c31aefcca3625d01dd964c8e12b997da71b865
Acoliver102 <55566341+Acoliver102@users.noreply.github.com>
2020-02-08 10:33:49 -0600

    command fixes

--- commands/climb/raise_lift.py
+++ commands/climb/raise_lift.py
@@ -5,19 +5,20 @@ class RaiseLift(Command):
     def __init__(self, distance):
         super.__init__('RaiseLift')
         self.distance = distance
+        self.requires(Climb())
 
     def initiate(self):
-        Climb.turn_ticks(distance)
-        Climb.state = Climb.ClimbState.RISING
+        Climb().turn_ticks(distance)
+        Climb().state = Climb().ClimbState.RISING
 
     def end(self):
-        Climb.state = Climb.ClimbState.RAISED
+        Climb().state = Climb().ClimbState.RAISED
 
     def execute(self):
         pass
 
     def isFinished(self):
-        if climb.climb_controller.get() == 0.0:
+        if Climb().climb_controller.get() == 0.0:
             return True
 
     
